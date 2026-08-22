cfb1cd4775ecd75953632d1177211793f9463758
Dfm007 <724625725@qq.com>
2026-08-22 08:47:00 +0800

    Force landscape at app level

--- iosmovie/iosmovieApp.swift
+++ iosmovie/iosmovieApp.swift
@@ -1,7 +1,16 @@
-﻿import SwiftUI
+import SwiftUI
+import UIKit
+
+class LandscapeAppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        .landscape
+    }
+}
 
 @main
 struct iosmovieApp: App {
+    @UIApplicationDelegateAdaptor(LandscapeAppDelegate.self) private var appDelegate
+
     var body: some Scene {
         WindowGroup {
             HomeView()
